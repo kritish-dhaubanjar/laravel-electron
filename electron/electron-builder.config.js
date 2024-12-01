e0f7718603a9ad4d96aab06b390b072c8a66a9ca
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2024-12-02 01:46:59 +0545

    fix(electron): update app -> application

--- electron/electron-builder.config.js
+++ electron/electron-builder.config.js
@@ -2,6 +2,6 @@ module.exports = {
   appId: 'laravel-electron',
   productName: 'Laravel-Electron',
   asar: false,
-  files: ['php-bin', 'assets', 'index.js', 'php.js', { from: 'app', to: 'app', filter: ['**/*'] }],
+  files: ['php-bin', 'assets', 'index.js', 'php.js', { from: 'application', to: 'application', filter: ['**/*'] }],
   icon: './assets/icon.png'
 }

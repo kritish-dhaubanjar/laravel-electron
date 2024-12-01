f8dd533b756def9978d03f55b4a6c4832868f08a
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2024-12-02 01:29:04 +0545

    chore(electron): update name and config

--- electron/electron-builder.config.js
+++ electron/electron-builder.config.js
@@ -2,6 +2,6 @@ module.exports = {
   appId: 'laravel-electron',
   productName: 'Laravel Electron',
   asar: false,
-  files: ['php-bin', 'assets', 'index.js', 'php.js', { from: "api", to: "api", filter: ["**/*"] }],
+  files: ['php-bin', 'assets', 'index.js', 'php.js', { from: 'app', to: 'app', filter: ['**/*'] }],
   icon: './assets/icon.png'
 }
